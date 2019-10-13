7999774b98d381212bc8a0f6b6ccd002989dbbff
Christopher De Vries <devries@idolstarastronomer.com>
2019-10-13 12:43:24 -0400

    Updated output of change program and made the emulator work at full speed if in non-verbose mode by default.

--- emulator.py
+++ emulator.py
@@ -12,7 +12,7 @@ def main(argv):
     parser.add_argument('rom_file', help='Assembled ROM file')
     parser.add_argument('-v', '--verbose', help='verbose output', action="store_true")
     parser.add_argument('-d', '--dump', help='range of memory to dump in format start:stop')
-    parser.add_argument('-s', '--sleep', help='sleep between iterations', type=float, default=0.01)
+    parser.add_argument('-s', '--sleep', help='sleep between iterations', type=float, default=-1.0)
     args = parser.parse_args()
 
     cpu = HackCpu()
@@ -22,6 +22,13 @@ def main(argv):
     cpu.loadRom(program)
     fin.close()
     i = 0
+    if args.sleep<0.0:
+        if args.verbose:
+            sleepytime = 0.01
+        else:
+            sleepytime = 0.0
+    else:
+        sleepytime = args.sleep
 
     try:
         for pc in cpu:
@@ -32,7 +39,7 @@ def main(argv):
             if args.verbose:
                 print "{:7d} -> {:5d}: {:016b}, A={:04x}, D={:04x}, SP={:5d} M(SP)={:5d}".format(i,pc,instruction,cpu.a,cpu.d,sp,cpu.peek(sp))
                 # print "    Memory of interest: 256:{:5d}, 263:{:5d}, 264:{:5d}, 265:{:5d}".format(cpu.peek(256),cpu.peek(263),cpu.peek(264),cpu.peek(265))
-            time.sleep(args.sleep)
+            time.sleep(sleepytime)
 
     except KeyboardInterrupt:
         print "Ending Program"
